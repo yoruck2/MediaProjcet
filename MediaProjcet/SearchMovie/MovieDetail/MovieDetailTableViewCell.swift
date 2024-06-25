675d82d2d98164756e4c5846e7a48a31c04b44c6
yoruck2 <yoruck2@gmail.com>
2024-06-26 03:33:35 +0900

    ✨ MovieDetailTableViewCell 구현

--- MediaProjcet/SearchMovie/MovieDetail/MovieDetailTableViewCell.swift
+++ MediaProjcet/SearchMovie/MovieDetail/MovieDetailTableViewCell.swift
@@ -5,4 +5,56 @@
 //  Created by dopamint on 6/25/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+class MovieDetailTableViewCell: UITableViewCell {
+    
+    let titleLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: 18)
+        $0.textColor = .white
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var movieCollectionView = UICollectionView(frame: .zero,
+                                                    collectionViewLayout: collectionViewLayout())
+    func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 120, height: 160)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 0
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        layout.scrollDirection = .horizontal
+        layout.collectionView?.showsVerticalScrollIndicator = false
+        return layout
+    }
+    
+    func configureHierarchy() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(movieCollectionView)
+        
+    }
+    
+    func configureLayout() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(contentView).inset(20)
+            make.height.equalTo(20)
+        }
+        // MARK: offset 주면 radius가 꺼짐??? -
+        movieCollectionView.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalTo(contentView)
+            make.top.equalTo(titleLabel.snp.bottom)
+        }
+    }
+}
