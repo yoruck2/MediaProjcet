f8f366ed974cacbba4fa0e86654b19ef5c7c1660
yoruck2 <yoruck2@gmail.com>
2024-06-26 03:33:17 +0900

    ✨ MovieDetailCollectionViewCell 구현

--- MediaProjcet/SearchMovie/MovieDetail/MovieDetailCollectionViewCell.swift
+++ MediaProjcet/SearchMovie/MovieDetail/MovieDetailCollectionViewCell.swift
@@ -13,9 +13,23 @@ class MovieDetailCollectionViewCell: UICollectionViewCell {
         $0.backgroundColor = .gray
     }
     
-    
-    override func layoutSubviews() {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(posterImageView)
+        posterImageView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView.safeAreaLayoutGuide)
+        }
         layer.cornerRadius = 10
         clipsToBounds = true
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    override func layoutSubviews() {
+//        layer.cornerRadius = 10
+//        clipsToBounds = true
+//    }
 }
