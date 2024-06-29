9d1da49c177026bb59a5c51fdedcc6e66645ee6b
yoruck2 <yoruck2@gmail.com>
2024-06-29 13:20:00 +0900

    ♻️ TMDBAPI  request model 파라미터 enum case 시도

--- MediaProjcet/Model/TMDBModel.swift
+++ MediaProjcet/Model/TMDBModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+//protocol TMDBModel {
+//    
+//}
+//
+//enum Model: TMDBModel {
+//    case searchedMovie
+//    case trending
+//    case movieSuggestion
+//    case movieImage
+//    
+//    func mm() -> TMDBModel {
+//        switch self {
+//        case .searchedMovie:
+//            return SearchedMovie.self as! TMDBModel
+//        case .trending:
+//            return SearchedMovie.self as! TMDBModel
+//        case .movieSuggestion:
+//            return SearchedMovie.self as! TMDBModel
+//        case .movieImage:
+//            return SearchedMovie.self as! TMDBModel
+//        }
+//    }
+//}
