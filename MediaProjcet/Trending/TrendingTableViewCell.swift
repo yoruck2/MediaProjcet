3368c2637e89fd42c3671f345cf95511a3eeb903
yoruck2 <yoruck2@gmail.com>
2024-07-01 03:29:56 +0900

    ✨ TrendingTableViewCell 에 통신 결과 반영하기

--- MediaProjcet/Trending/TrendingTableViewCell.swift
+++ MediaProjcet/Trending/TrendingTableViewCell.swift
@@ -7,18 +7,24 @@
 
 import UIKit
 
+import Kingfisher
 import SnapKit
 import Then
 
 class TrendingTableViewCell: BaseTableViewCell {
     
-    var cellData: Trending.Result?
+    var movieData: Trending.Result? {
+        didSet {
+            configureView()
+        }
+    }
+    var castData: MovieCredits?
     
     lazy var dateLabel = UILabel().then {
-        $0.text = "asd"
+        $0.textColor = .lightGray
     }
     lazy var genreLabel = UILabel().then {
-        $0.text = "asd"
+        $0.text = "#장르"
         $0.font = Font.bold20
     }
     lazy var trendView = TrendView()
@@ -48,6 +54,22 @@ class TrendingTableViewCell: BaseTableViewCell {
     }
     override func configureView() {
         selectionStyle = .none
-        
+        guard let backdropPath = movieData?.backdropPath else {
+            return
+        }
+        let url = URL.make(with: "https://image.tmdb.org/t/p/original\(backdropPath)")
+        dateLabel.text = movieData?.releaseDate
+        trendView.backImageView.kf.setImage(with: url)
+        trendView.rateValueView.text = " \(round((movieData?.voteAverage ?? 0) * 10) / 10) "
+        trendView.titleLabel.text = movieData?.title
+        trendView.castLabel.text = ""
+        for i in 0...4 {
+            guard var labelText = trendView.castLabel.text,
+                    var name = castData?.cast?[i].name else {
+                return
+            }
+            labelText += name + ", "
+            trendView.castLabel.text = labelText
+        }
     }
 }
