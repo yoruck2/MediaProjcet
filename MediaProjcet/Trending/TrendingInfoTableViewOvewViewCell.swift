a8d7ccad509c853c32788a58d6f87245f6399c36
yoruck2 <yoruck2@gmail.com>
2024-07-01 21:46:36 +0900

    ✨ TrendingInfoTableViewOverViewCell 구현

--- MediaProjcet/Trending/TrendingInfoTableViewOvewViewCell.swift
+++ MediaProjcet/Trending/TrendingInfoTableViewOvewViewCell.swift
@@ -5,4 +5,47 @@
 //  Created by dopamint on 7/1/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+protocol ReadmoreButtonDelegate {
+    func toggleReadme()
+}
+
+class TrendingInfoTableViewOverViewCell: BaseTableViewCell {
+    
+    var delegate: ReadmoreButtonDelegate?
+    
+    let overViewLabel = UILabel().then {
+        $0.numberOfLines = 0
+    }
+    lazy var readmoreButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        $0.setImage(UIImage(systemName: "chevron.up"), for: .selected)
+        $0.addTarget(self, action: #selector(readmeMoreButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc
+    func readmeMoreButtonTapped() {
+        readmoreButton.isSelected.toggle()
+        delegate?.toggleReadme()
+    }
+    
+    override func configureHierarchy() {
+        contentView.addSubview(overViewLabel)
+        contentView.addSubview(readmoreButton)
+    }
+    override func configureLayout() {
+        overViewLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(10)
+            make.horizontalEdges.equalToSuperview().inset(20)
+            make.bottom.equalTo(readmoreButton.snp.top)
+        }
+        readmoreButton.snp.makeConstraints { make in
+            make.height.equalTo(15)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview().inset(5)
+        }
+    }
+}
